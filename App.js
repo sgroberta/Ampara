9cead9c788e41f757ec43cbb4ed7119daa877682
Roberta <2023302946@aluno.riogrande.ifrs.edu.br>
2026-03-18 22:07:25 -0300

    testes iniciais

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+   return (
+      <View style={styles.container}>
+          <Text style={styles.text}>Olá, React Native!</Text>
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({
+   container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+    },
+    text: {
+        fontSize: 20,
+        color: 'blue',
+    },
+});
+    
